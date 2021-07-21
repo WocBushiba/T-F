d43e67df453a1e11f90352cf9d1f8d73a11b1a19
WocBushiba <1804398992@qq.com>
2021-07-21 14:31:16 +0800

    Content Fill

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobi
 else
 {
 }
+for (let index = 0; index < 5; index++) {
+    const element = 5[index];
+    var span = document.createElement("span");
+    span.setAttribute("")
+    var container = document.getElementById("background");
+    alert(span);
+    container.insertAdjacentElement("afterbegin",span)
+}

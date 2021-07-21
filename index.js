8ead20c0e945acdc68df3db0a642e12eaafa60c2
WocBushiba <1804398992@qq.com>
2021-07-21 14:44:03 +0800

    Content Fill

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ else
 for (let index = 0; index < 5; index++) {
     const element = 5[index];
     var span = document.createElement("span");
-    span.setAttribute("")
+    //span.setAttribute("")
     var container = document.getElementById("background");
     alert(span);
     container.insertAdjacentElement("afterbegin",span)

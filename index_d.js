6d911309582deb82b84af05e020e243042cc96fc
WocBushiba <1804398992@qq.com>
2021-07-21 15:57:48 +0800

    index correction

---
+++ index_d.js
@@ -0,0 +1,8 @@
+for (let index = 0; index < 5; index++) {
+    const element = 5[index];
+    var span = document.createElement("span");
+    //span.setAttribute("")
+    var container = document.getElementById("background");
+    //alert(span);
+    container.insertAdjacentElement("afterbegin",span)
+}

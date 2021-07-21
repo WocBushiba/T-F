bf5b054800019bc2ad38522142d8ce633758556b
WocBushiba <1804398992@qq.com>
2021-07-21 16:45:38 +0800

    index correction

--- index_d.js
+++ index_d.js
@@ -1,8 +1,2 @@
-for (let index = 0; index < 5; index++) {
-    const element = 5[index];
-    var span = document.createElement("span");
-    //span.setAttribute("")
-    var container = document.getElementById("background");
-    //alert(span);
-    container.insertAdjacentElement("afterbegin",span)
-}
+var container = document.getElementById("background");
+container.style.height = document.getElementById("Container");
